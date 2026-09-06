0ef2251dffb61c473807d21b528907cbcdb8cac2
ark3127 <aman.kawale47@gmail.com>
2026-09-06 14:09:46 +0530

    Add widget test for SYSTEM-K title display

---
+++ test/widget_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:system_k/main.dart';
+
+void main() {
+  testWidgets('App shows SYSTEM-K title', (WidgetTester tester) async {
+    await tester.pumpWidget(const SystemKApp());
+
+    expect(find.text('SYSTEM-K'), findsOneWidget);
+  });
+}
