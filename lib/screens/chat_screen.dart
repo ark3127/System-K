cb8f23ab052a0c3dd93500623b730d46bb54a454
ark3127 <aman.kawale47@gmail.com>
2026-09-06 14:27:51 +0530

    Create ChatScreen widget for chat interface

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SYSTEM-K'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings_outlined),
+            onPressed: () {
+              Navigator.of(context).pushNamed('/settings');
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Chat coming soon'),
+      ),
+    );
+  }
+}
